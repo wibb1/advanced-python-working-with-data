1cdaa590c7fb816f7668944f8c008b0e5843937d
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:18:23 -0400

    sorting

--- Start/Ch_1/sorting.py
+++ Start/Ch_1/sorting.py
@@ -4,25 +4,37 @@
 import json
 
 
-numbers = [42, 54, 19, 17, 23, 31, 16, 4]
-names = ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "Stacy"]
+# numbers = [42, 54, 19, 17, 23, 31, 16, 4]
+# names = ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "Stacy"]
+# print("start")
+# print(numbers)
+# print(names)
+# # TODO: the sorted() function can be used to return a new list with sorted data
+# new_numbers = sorted(numbers)
+# new_names = sorted(names)
+# print("sorted")
+# print(new_numbers)
+# print(new_names)
+# # TODO: alternately, you can use the list object's sort() method, which sorts the list in-place
+# print("reversed")
+# new_numbers.sort(reverse=True)
+# new_names.sort(reverse=True)
 
-# TODO: the sorted() function can be used to return a new list with sorted data
-
-
-# TODO: alternately, you can use the list object's sort() method, which sorts the list in-place
-
-
-# TODO: To sort custom objects, we can tell the sort function which property to use
-# by specifying a key function
+# print(new_numbers)
+# print(new_names)
+# # TODO: To sort custom objects, we can tell the sort function which property to use
+# # by specifying a key function
 
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
 
+def get_mag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
 
-# def getmag(dataitem):
-#     magnitude = dataitem["properties"]["mag"]
-#     if (magnitude is None):
-#         magnitude = 0
-#     return float(magnitude)
+data['features'].sort(key=get_mag, reverse=True)
+for i in range (0, 10):
+    print(data['features'][i]['properties']['place'])
