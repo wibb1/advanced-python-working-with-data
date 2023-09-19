54d194dedfc25c330fd0da22dfe47bf5867860d0
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:19:45 -0400

    named tuples

--- Start/Ch_2/namedtuple.py
+++ Start/Ch_2/namedtuple.py
@@ -4,5 +4,16 @@ import collections
 
 
 # TODO: create a Point namedtuple
+Point = collections.namedtuple("Point", "x y")
+
+p1 = Point(10,20)
+p2 = Point(30,40)
+
+print(p1, p2) # prints in a readable format
+print(p1.x, p1.y) # still able to access the values individually
+
 
 # TODO: use _replace to create a new instance
+
+p1 = p1._replace(x=100) # replace an individual element of the tuple
+print(p1)
