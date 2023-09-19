a463eca1e4a2eeaf61e3d2b5b98d5c9b0eec9dbb
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:19:05 -0400

    challenge

--- Start/Ch_1/challenge_start.py
+++ Start/Ch_1/challenge_start.py
@@ -11,6 +11,54 @@ import json
 # 4: Print the top 10 most significant events, with the significance value of each
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+def filter_felt(q):
+    if q['properties']['felt'] is None or q['properties']['felt'] <= 100:
+        return False
+    else:
+        return True
+
+def get_felt(quake):
+    felt=quake['properties']['felt']
+    if felt is None:
+        return float(0)
+    else:
+        return float(felt)
     
+def get_sig(quake):
+    sig = quake['properties']['sig']
+    if sig is None:
+        return 0
+    else:
+        return sig
+
+quakes = data['features']
+felt_by_100_list = list(filter(filter_felt, quakes))
+felt_by_100_list.sort(key=get_felt, reverse=True)
+
+print(f"1. Total quakes: {len(quakes)}")
+print(f"2. Quakes Felt by >= 100 people: {len(felt_by_100_list)}")
+print(f"3. most felt by = {felt_by_100_list[0]['properties']['place']} reports: {felt_by_100_list[0]['properties']['felt']}")
+
+
+def get_sig(dataitem):
+    significance = dataitem["properties"]["sig"]
+    if (significance is None):
+        significance = 0
+    return float(significance)
+
+def transform_sig(quake):
+    q = quake['properties']
+    return {
+        "magnitude": q['mag'],
+        "place": q['place'],
+        "significance": q['sig']
+    }
+
+quakes.sort(key=get_sig, reverse=True)
+sig_list = quakes
+print('4. Most significant events \n')
+for i in range(0,10):
+    print(f"Event: Magnitude: {sig_list[i]['properties']['mag']} \nLocation: {sig_list[i]['properties']['place']} \nSignificance: {sig_list[i]['properties']['sig']}")
