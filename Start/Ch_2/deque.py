2f29d7c29474268bb95ce786b4d0615972448318
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:20:28 -0400

    deques

--- Start/Ch_2/deque.py
+++ Start/Ch_2/deque.py
@@ -4,12 +4,22 @@ import collections
 import string
 
 
-# TODO: initialize a deque with lowercase letters
+# TODO: initialize a deque (pronounced 'deck') with lowercase letters
+d = collections.deque(string.ascii_lowercase)
 
 # TODO: deques support the len() function
-
+print(f"Item count: {len(d)}")
 # TODO: deques can be iterated over
-
+for letter in d:
+    print(letter.upper())
+    
 # TODO: manipulate items from either end
+d.pop()
+d.popleft()
+d.append(2)
+d.appendleft(1)
+print(d)
 
 # TODO: use an index to get a particular item
+d.rotate(1)
+print(d)
