704e46781dd26a600f5e5e2ef6c4334d59b86cdb
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:17:41 -0400

    min and max

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -4,19 +4,33 @@ import json
 
 
 # Declare an array with some sample data in it
-values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
-strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
+# values = [3.0, 2.5, 5.1, 4.1, 1.8, 1.6, 2.2, 5.7, 6.1]
+# strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
+# print(f"The minimum value is: {min(values)}")
+# print(f"The minimum value is: {min(strings)}")
 
 # TODO: The max() function finds the maximum value
-
+# print(f"The maximum value is: {max(values)}")
+# print(f"The maximum value is: {max(strings)}")
 
 # TODO: define a custom "key" function to extract a data field
-
+# print(f"The minimum value is: {min(strings, key=len)}")
+# print(f"The maximum value is: {max(strings, key=len)}")
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]["title"])
+
+def get_mag(data_item):
+    magnitude = data_item["properties"]["mag"]
+    if magnitude is None:
+        magnitude = 0
+    return float(magnitude)
+
+print(f"The minimum magnitude earthquake is: {min(data['features'], key=get_mag)}")
+print(f"The maximum magnitude earthquake is: {max(data['features'], key=get_mag)}")
