ecab6f55859d0e850aa310e7f7ed0afaa9160f2c
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:20:40 -0400

    challenge 2

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,25 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+from collections import defaultdict
+from pprint import PrettyPrinter
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+# use the event type to classify and count the number of occurrences of each in the following format
+# earthquake      : 11558
+# explosion       : 85
+# quarry blast    : 81
+# ice quake       : 17
+# other event     : 4
+
+quakeCounter = defaultdict(int)
+
+for quake in data['features']:
+    quakeCounter[quake['properties']['type']] += 1
+    
+for type in quakeCounter:
+    tab = '\t'
+    print(f"{type}{tab}: {quakeCounter[type]}")
