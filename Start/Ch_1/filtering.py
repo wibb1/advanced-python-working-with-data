af6bc46a23226498ec45f8be688d3a8cb71ef262
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:18:35 -0400

    filtering

--- Start/Ch_1/filtering.py
+++ Start/Ch_1/filtering.py
@@ -4,29 +4,44 @@
 import json
 
 
-def filterEvens(x):
-    # filters out even numbers and keeps odd numbers
-    if x % 2 == 0:
-        return False
-    return True
+# def filterEvens(x):
+#     # filters out even numbers and keeps odd numbers
+#     if x % 2 == 0:
+#         return False
+#     return True
 
 
-def filterUppers(x):
-    # filters out upper-case letters and keeps lower case letters
-    if x.isupper():
-        return False
-    return True
+# def filterUppers(x):
+#     # filters out upper-case letters and keeps lower case letters
+#     if x.isupper():
+#         return False
+#     return True
 
 
-# define some sample sequences to operate on
-nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
-chars = "abcDeFGHiJklmnoP"
+# # define some sample sequences to operate on
+# nums = (1, 8, 4, 5, 13, 26, 381, 410, 58, 47)
+# chars = "abcDeFGHiJklmnoP"
 
-# TODO: use filter to remove items from a list
-
-# TODO: use filter on non-numeric sequence
+# # TODO: use filter to remove items from a list
+# odds = list(filter(filterEvens, nums))
+# uppers = list(filter(filterUppers, chars))
+# print(uppers)
+# print(odds)
+# # TODO: use filter on non-numeric sequence
 
 # Use the filter on our data - let's filter out all seismic events that were *not* quakes
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+def not_a_quake(q):
+    if q['properties']['type'] == "earthquake":
+        return False
+    else:
+        return True
+    
+events = list(filter(not_a_quake, data['features']))
+
+print(f"Sum of non quake events {len(events)}")
+for i in range(0,10):
+    print(events[i]['properties']['type'])
