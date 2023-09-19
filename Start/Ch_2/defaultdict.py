3109c79067681791d1ca3f20d58d310a3eee7c7f
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-19 19:20:03 -0400

    default dictionaries

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -8,10 +8,12 @@ fruits = ['apple', 'pear', 'orange', 'banana',
           'apple', 'grape', 'banana', 'banana']
 
 # TODO: use a dictionary to count each element
-fruitCounter = dict()
-
-# TODO: Count the elements in the list
+# fruitCounter = defaultdict(int) # starts at 0
+fruitCounter = defaultdict(lambda: 100) #starts at 100
+# TODO: Count the elements in the list - defaultdict eliminates the need to check for the presence 
+#       of the key before it is created or used 
 for fruit in fruits:
     fruitCounter[fruit] += 1
 
 # TODO: print the result
+print(fruitCounter)
