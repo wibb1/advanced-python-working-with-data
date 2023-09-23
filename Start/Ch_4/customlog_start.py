4d6964fe0ff6222745e865e9acd58e6b442efd99
Will Campbell <62625260+wibb1@users.noreply.github.com>
2023-09-23 14:22:45 -0400

    logging 2

--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -3,13 +3,21 @@
 import logging
 
 # TODO: add another function to log from
+def log_a_message():
+    logging.info("Info level debugger in the log_a_message function", extra=other_log_data)
 
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
+log_str = "User: %(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+log_date = "%m/%d/%Y %I:%M:%S %p"
+other_log_data = {"user": "willc@example.com"}
+logging.basicConfig(filename="output.log",
+                    level=logging.DEBUG,
+                    format=log_str,
+                    datefmt=log_date)
+log_a_message()
+logging.info("This is an info-level log message", extra=other_log_data)
+logging.warning("This is a warning-level message", extra=other_log_data)
 
